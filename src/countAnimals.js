2b944a54799ea83458e9b51ff1456c78643247f2
WBomfim <willianbomfim@hotmail.com>
2022-03-17 00:19:40 -0300

    Adicionado requisito 5, criado a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const countAnimalsAll = () => {
+  const totalAnimals = {};
+  species.forEach((animal) => {
+    totalAnimals[animal.name] = animal.residents.length;
+  });
+  return totalAnimals;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return countAnimalsAll();
+  }
+
+  const specieAnimals = species.find((specie) => specie.name === animal.specie).residents;
+
+  if (animal.sex !== undefined) {
+    return specieAnimals.filter((specieAnimal) => specieAnimal.sex === animal.sex).length;
+  }
+  return specieAnimals.length;
 }
 
 module.exports = countAnimals;
