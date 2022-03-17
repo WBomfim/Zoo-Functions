5116446b5da1da899a51034ebc304f4e5b03de0d
WBomfim <willianbomfim@hotmail.com>
2022-03-17 18:43:04 -0300

    Criado as funções que executam os filtros solicitados no requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,101 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const includeNamesFalse = () => {
+  const filterAnimal = {};
+
+  species.forEach((animal) => {
+    filterAnimal[animal.location] = [];
+  });
+
+  Object.keys(filterAnimal).forEach((key) => species.forEach((animal) => {
+    if (animal.location === key) {
+      filterAnimal[key].push(animal.name);
+    }
+  }));
+
+  return filterAnimal;
+};
+
+const includeNamesTrue = () => {
+  const filterAnimal = {};
+
+  species.forEach((animal) => {
+    filterAnimal[animal.location] = [];
+  });
+
+  Object.keys(filterAnimal).forEach((key) => species.forEach((animal) => {
+    const animalNames = {};
+    if (animal.location === key) {
+      animalNames[animal.name] = animal.residents.map((resident) => resident.name);
+      filterAnimal[key].push(animalNames);
+    }
+  }));
+
+  return filterAnimal;
+};
+
+const includeNamesTrueSorted = () => {
+  const filterAnimal = {};
+
+  species.forEach((animal) => {
+    filterAnimal[animal.location] = [];
+  });
+
+  Object.keys(filterAnimal).forEach((key) => species.forEach((animal) => {
+    const animalNames = {};
+    if (animal.location === key) {
+      animalNames[animal.name] = animal.residents.map((resident) => resident.name).sort();
+      filterAnimal[key].push(animalNames);
+    }
+  }));
+
+  return filterAnimal;
+};
+
+const includeNamesTrueSex = () => {
+  const filterAnimal = {};
+
+  species.forEach((animal) => {
+    filterAnimal[animal.location] = [];
+  });
+
+  Object.keys(filterAnimal).forEach((key) => species.forEach((animal) => {
+    const animalNames = {};
+    if (animal.location === key) {
+      animalNames[animal.name] = animal.residents.filter((resident) =>
+        resident.sex === 'female').map((residentsCurr) => residentsCurr.name);
+      filterAnimal[key].push(animalNames);
+    }
+    console.log(animalNames);
+  }));
+
+  return filterAnimal;
+};
+
+const includeNamesTrueSortedSex = () => {
+  const filterAnimal = {};
+
+  species.forEach((animal) => {
+    filterAnimal[animal.location] = [];
+  });
+
+  Object.keys(filterAnimal).forEach((key) => species.forEach((animal) => {
+    const animalNames = {};
+    if (animal.location === key) {
+      animalNames[animal.name] = animal.residents.filter((resident) =>
+        resident.sex === 'female').map((residentsCurr) => residentsCurr.name).sort();
+      filterAnimal[key].push(animalNames);
+    }
+    console.log(animalNames);
+  }));
+
+  return filterAnimal;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  
 }
 
 module.exports = getAnimalMap;
