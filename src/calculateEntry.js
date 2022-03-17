77d8204849d4d21b1943aa520662686246caa9af
WBomfim <willianbomfim@hotmail.com>
2022-03-17 01:50:32 -0300

    Adicionado requisito 6, criado a função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    adult: entrants.filter((people) => people.age >= 18 && people.age < 50).length,
+    child: entrants.filter((people) => people.age < 18).length,
+    senior: entrants.filter((people) => people.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (Array.isArray(entrants) === false) {
+    return 0;
+  }
+
+  const visits = countEntrants(entrants);
+  const { prices } = data;
+  const adultValue = prices.adult * visits.adult;
+  const childValue = prices.child * visits.child;
+  const seniorValue = prices.senior * visits.senior;
+
+  return adultValue + childValue + seniorValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
