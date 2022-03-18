f7dbf3521e82e6a948788354fb1ec723375ec40f
WBomfim <willianbomfim@hotmail.com>
2022-03-17 23:22:38 -0300

    Adicionado requisito 9, criado a função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { employees, species } = data;
+  return Object.values(species.find((animal) => animal.id === employees.find((employee) =>
+    employee.id === id).responsibleFor[0]).residents.reduce((acc, curr) =>
+    (acc.age < curr.age ? curr : acc)));
 }
 
 module.exports = getOldestFromFirstSpecies;
