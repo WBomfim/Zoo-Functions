026371b3fc5452fbd539feb0f5c962ca7d8400d9
WBomfim <willianbomfim@hotmail.com>
2022-03-19 19:13:26 -0300

    Adicionado requisito 10, criado a função getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+const getAnimal = (employeeId) =>
+  employees.find((employee) => employee.id === employeeId).responsibleFor
+    .map((specieId) => species.find((specie) => specie.id === specieId).name);
+
+const getLocationOfAnimales = (speciesNames) =>
+  speciesNames.map((specieName) => species
+    .find((specie) => specie.name === specieName).location);
+
+const employeeDataGenerator = (employeeIdentification) =>
+  employees.reduce((objectData, employee) => {
+    const object = objectData;
+    if (Object.values(employee).includes(employeeIdentification)) {
+      object.id = employee.id;
+      object.fullName = `${employee.firstName} ${employee.lastName}`;
+      object.species = getAnimal(employee.id);
+      object.locations = getLocationOfAnimales(getAnimal(employee.id));
+    }
+    return objectData;
+  }, {});
+
+const checkIdentification = (employeeIdentification) => {
+  const identifocation = Object.values(employeeIdentification)[0];
+  return employees.some((employee) => Object.values(employee).includes(identifocation));
+};
+
+function getEmployeesCoverage(employeeIdentification) {
+  if (!employeeIdentification) {
+    return employees.reduce((employeesData, employee) => {
+      employeesData.push(employeeDataGenerator(employee.id));
+      return employeesData;
+    }, []);
+  }
+  if (checkIdentification(employeeIdentification)) {
+    const identifocation = Object.values(employeeIdentification)[0];
+    return employeeDataGenerator(identifocation);
+  }
+  throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
